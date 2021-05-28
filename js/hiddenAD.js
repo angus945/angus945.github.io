c399dc04b9b335c6bf5274c25d68935cbbb873e1
DESKTOP-OH6EQB8\angus <angus9456@gmail.com>
2021-05-28 19:17:23 +0800

    hide ad

---
+++ js/hiddenAD.js
@@ -0,0 +1,10 @@
+(function($){
+    setInterval(() => {
+        $.each($('iframe'), (arr,x) => {
+            let src = $(x).attr('src');
+            if (src && src.match(/(ads-iframe)|(disqusads)/gi)) {
+                $(x).remove();
+            }
+        });
+    }, 300);
+})(jQuery);
